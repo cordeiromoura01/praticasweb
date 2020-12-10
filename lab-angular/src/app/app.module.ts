66cf438cc417e9ff96ece193dc981303cf189bf3
Cordeiro Moura <cordeiromouratst@gmail.com>
2020-12-10 18:35:54 -0300

    atualizando

--- lab-angular/src/app/app.module.ts
+++ lab-angular/src/app/app.module.ts
@@ -4,11 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { ContadorComponent } from './contador/contador.component';
+import { CardComponent } from './card/card.component';
+import { FormReativoComponent } from './form-reativo/form-reativo.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormacontatoComponent } from './formacontato/formacontato.component';
+import { FormcontatoComponent } from './formcontato/formcontato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RodapeComponent
+    RodapeComponent,
+    ContadorComponent,
+    CardComponent,
+    FormReativoComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    FormacontatoComponent,
+    FormcontatoComponent
   ],
   imports: [
     BrowserModule,
---
+++ lab-angular/src/app/card/card.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  carros: any[] = [
+    {
+      "name": "BMW",
+      "age": 12,
+      "color": 'blue'
+    },
+    {
+      "name": "Ford",
+      "age": 15,
+      "color": 'yellow'
+    },
+    {
+      "name": "Suzuki",
+      "age": 18,
+      "color": 'silver'
+    },
+    {
+      "name": "MG Hector",
+      "age": 14,
+      "color": 'red'
+    },
+    {
+      "name": "Jaguar",
+      "age": 8,
+      "color": 'green'
+    }
+  ];
+
+  
+  public lista = [
+    {
+      id: '1',
+      year:'2007',
+      cert: 'SCJP 1.5',
+    },
+    {
+      id: '2',
+      year:'2009',
+      cert: 'SCWCD 1.5',
+    },
+    {
+      id: '3',
+      year:'2011',
+      cert: 'CTFL',
+    }];
+}
